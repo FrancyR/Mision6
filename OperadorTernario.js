cc9d0c7bcdab1b8df4e7cd4aa7b74fa91fb483c7
claer <francy.ruales@correounivalle.edu.co>
2023-06-17 08:04:36 -0500

    Crear tres carpetas

---
+++ OperadorTernario.js
@@ -0,0 +1,2 @@
+let number = 12;
+number == 8? console.log ("El número es igual a 8"):console.log("El número no es 8");
---
+++ Saludando.js
@@ -0,0 +1,9 @@
+let time = 13;
+if (time < 12){
+    console.log("Buenos dias");
+} else if (time>=12 && time <=18){
+    console.log("buenas tardes");
+} else {
+    console.log ("Buenas noches");
+}
+
